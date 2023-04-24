9dc913ee20999c1878c1daa0838e39a1dc1b3eaf
IvanPopovic1991 <iki.popovic991@gmail.com>
2023-04-24 14:11:57 +0100

    Added product to open trades position - Buy and sell product flow : - created Buy product page - elements and methods - created Sell product page - elements and methods - added parameters for products in TestNg.xml file - generated report after tests running (http://100.81.142.111:49782/index.html#behaviors/b1a8273437954620fa374b796ffaacdd/e66b180ab843d507/)

--- src/test/java/Pages/ProductsPage.java
+++ src/test/java/Pages/ProductsPage.java
@@ -6,8 +6,6 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
-import static java.awt.SystemColor.text;
-
 public class ProductsPage extends BasePage{
 
     public ProductsPage(WebDriver driver) {
@@ -17,8 +15,6 @@ public class ProductsPage extends BasePage{
     }
     @FindBy(xpath = "//div[@class='viewLayer']//..//select[1]")
     public WebElement amountField;
-    @FindBy(xpath = "")
-    WebElement product;
 
    @FindBy(xpath = "//div[@class='ticketActionButton t_button primary'  and contains(text(),'BUY now')]")
     public WebElement buyButton;
@@ -26,12 +22,12 @@ public class ProductsPage extends BasePage{
     @FindBy(xpath = "//div[@class='ticketActionButton t_button primary' and contains(text(),'SELL now')]")
     public WebElement sellButton;
 
-    String productCart = "//div[@class='TradesZone']//div/span[@class='webSymbol' and contains(text(),$$)]";
+    String productInCart = "//div[@class='TradesZone']//div/span[@class='webSymbol' and contains(text(),$$)]";
+
 
     public void checkProductsAreDisplayed(String... products){
         for (String product : products){
-            getText(By.xpath(productCart.replace("$$",product)),""+product);
+            getText(By.xpath(productInCart.replace("$$",product)),""+product);
         }
     }
-
 }
